b6be14d92c1b0104a532679bf0c4212b28011b99
jooaquinn <jcenderelli@mail.austral.edu.ar>
2026-04-19 17:13:47 -0300

    Update exercise_remove_elements.py

--- exercise_remove_elements.py
+++ exercise_remove_elements.py
@@ -12,3 +12,10 @@ def remove_elements(lista):
         La lista después de remover los elementos indicados
     """
     pass  # Reemplazar con tu implementación
+    if len(lista) > 5:
+        lista.pop(5)
+    if len(lista) > 4:
+        lista.pop(4)
+    if len(lista) > 0:
+        lista.pop(0)
+    return lista
